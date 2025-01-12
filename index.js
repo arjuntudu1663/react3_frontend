311a064d5068a9c82185875ce062ee616b79499a
arjuntudu1663 <arjuntudu9163@gmail.com>
2025-01-12 11:48:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 try{
-    mongoose.connect("mongodb+srv://arjuntudu9163:awHhKuynNRCy47mD@cluster0.cq6wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb+srv://arjuntudu9163:fv9FIKG1eb8UKcee@cluster0.cq6wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 }catch(e){
     if(e){
         console.log("database connection error",e);
